ee6a78f599447df004f22111303dc6dce04278da
Randall Hale <culdesac@Mick-Donald-2.local>
2025-09-07 14:17:24 -0700

    moving icons

--- src/app/border/border.component.ts
+++ src/app/border/border.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { NavigationService } from '../services/navigation.service';
 
 @Component({
   selector: 'border',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './border.component.css'
 })
 export class BorderComponent {
+  @ViewChildren('icon img', { read: ElementRef }) images!: QueryList<ElementRef>;
+  private intervalId: any;
+  state: 'state1' | 'state2' = 'state1';
+
+  ngOnInit(): void {
+    // Start the wobble effect
+    this.startWobbling();
+  }
+
+  start(): void {
+    this.state = this.state == 'state1' ? 'state2' : 'state1';
+  }
+
+  startWobbling(): void {
+    this.intervalId = setInterval(() => {
+      this.images.forEach((img) => {
+        const randomRotation = this.getRandomRotation(-15, 15); // Random angle between -15 and 15 degrees
+        img.nativeElement.style.transform = `rotate(${randomRotation}deg)`;
+      });
+      console.log('test');
+    }, 500);
+  }
+
+  private getRandomRotation(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
 
 }
