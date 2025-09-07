efede6b29be0d3acefa9e062fd7d7fdc8dac9dc2
Randall Hale <culdesac@Mick-Donald-2.local>
2025-09-07 12:45:49 -0700

    dynamic border component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SlidesComponent } from './window/slides/slides.component';
+import { LandingComponent } from './landing/landing.component';
+import { BorderComponent } from './border/border.component';
 import { ProgressBarComponent } from './interactive/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SlidesComponent, ProgressBarComponent],
+  imports: [CommonModule, RouterOutlet, SlidesComponent, LandingComponent, BorderComponent, ProgressBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/border/border.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'border',
+  standalone: true,
+  imports: [],
+  templateUrl: './border.component.html',
+  styleUrl: './border.component.css'
+})
+export class BorderComponent {
+
+}
