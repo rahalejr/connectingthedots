ee3158892111eb6aaa91c142f679b346a5db15e7
Randall Hale <culdesac@Mick-Donald-2.local>
2025-09-19 09:55:28 -0700

    tides progress

--- src/app/directives/drag.directive.ts
+++ src/app/directives/drag.directive.ts
@@ -6,12 +6,12 @@ import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from
 })
 export class DragDirective {
   @Input() range: number[] = [0, 100];
-  @Input() svg!: SVGSVGElement;
+  @Input() svg!: ElementRef;
   @Input() direction: 'x' | 'y' = 'x';
   @Input() axis_constant = 0;
 
   @Output() coordinate = new EventEmitter<number>();
-  @Output() pixels = new EventEmitter<number[]>();
+  @Output() pixels = new EventEmitter<{x: number, y: number}>();
 
   private dragging = false;
   private default = 50;
@@ -19,9 +19,10 @@ export class DragDirective {
   constructor(private el: ElementRef<HTMLElement>) {}
 
   ngAfterViewInit() {
+    this.svg = this.svg.nativeElement;
     const initCoords = this.direction == 'x' ? this.svgToClient(this.default, this.axis_constant) : this.svgToClient(this.axis_constant, this.default);
     if (initCoords) {
-        this.pixels.emit([initCoords.x, initCoords.y]);
+        this.pixels.emit({x:initCoords.x, y:initCoords.y});
     }
   }
 
@@ -46,38 +47,44 @@ export class DragDirective {
   onMouseMove(e: MouseEvent) {
     if (!this.dragging || !this.svg) return;
 
-    const pt = this.svg.createSVGPoint();
-    pt.x = e.clientX;
-    pt.y = e.clientY;
-    const ctm = this.svg.getScreenCTM();
-    if (!ctm) return;
-    const svgCoords = pt.matrixTransform(ctm.inverse());
-
-    let val: number;
-    let fixedX: number;
-    let fixedY: number;
-
-    if (this.direction === 'x') {
-      val = Math.min(Math.max(svgCoords.x, this.range[0]), this.range[1]);
-      fixedX = val;
-      fixedY = this.axis_constant;
-    } else {
-      val = Math.min(Math.max(-svgCoords.y, this.range[0]), this.range[1]);
-      fixedX = this.axis_constant;
-      fixedY = val;
+    if (this.svg instanceof SVGSVGElement) {
+      const pt = this.svg.createSVGPoint();
+      pt.x = e.clientX;
+      pt.y = e.clientY;
+      const ctm = this.svg.getScreenCTM();
+      if (!ctm) return;
+      const svgCoords = pt.matrixTransform(ctm.inverse());
+  
+      let val: number;
+      let fixedX: number;
+      let fixedY: number;
+  
+      if (this.direction === 'x') {
+        val = Math.min(Math.max(svgCoords.x, this.range[0]), this.range[1]);
+        fixedX = val;
+        fixedY = this.axis_constant;
+      } else {
+        val = Math.min(Math.max(-svgCoords.y, this.range[0]), this.range[1]);
+        fixedX = this.axis_constant;
+        fixedY = val;
+      }
+  
+      this.coordinate.emit(val);
+  
+      const fixedPt = new DOMPoint(fixedX, fixedY).matrixTransform(ctm);
+      this.pixels.emit({x: fixedPt.x, y: fixedPt.y});
     }
-
-    this.coordinate.emit(val);
-
-    const fixedPt = new DOMPoint(fixedX, fixedY).matrixTransform(ctm);
-    this.pixels.emit([fixedPt.x, fixedPt.y]);
   }
 
   svgToClient(x: number, y: number) {
-    const ctm = this.svg.getScreenCTM();
-    if (!ctm) return null;
-    const pt = new DOMPoint(x, y);
-    const res = pt.matrixTransform(ctm);
-    return { x: res.x, y: res.y };
+    if (this.svg instanceof SVGSVGElement) {
+      const ctm = this.svg.getScreenCTM();
+      if (!ctm) return null;
+      const pt = new DOMPoint(x, y);
+      const res = pt.matrixTransform(ctm);
+      return { x: res.x, y: res.y };
+    }
+    return {x:0, y:0}
   }
+
 }
--- src/app/interactive/tides/tides.component.ts
+++ src/app/interactive/tides/tides.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Output, output, ViewChild } from '@angular/core';
+import { Component, ElementRef, Output, output, viewChild, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArrowComponent } from '../../interface/arrow/arrow.component';
 import { NavigationService } from '../../services/navigation.service';
@@ -18,11 +18,19 @@ export class TidesComponent {
   @ViewChild('animShift') tide_shift!: ElementRef<SVGAnimateElement>;
   @ViewChild('animRevert') tide_revert!: ElementRef<SVGAnimateElement>;
   @ViewChild('animMoon') moon_orbit!: ElementRef<SVGAnimateElement>;
+  @ViewChild('earth') earth_el!: ElementRef<HTMLElement>;
+  @ViewChild('moon') moon_el!: ElementRef<HTMLElement>;
+  @ViewChild('ocean') ocean_el!: ElementRef<HTMLElement>;
+  @ViewChild('svg_frame') frame!: SVGSVGElement;
+  @ViewChild('svg_frame') frame_el!: ElementRef;
 
   wrapper!: HTMLElement | null;
-  frame!: HTMLElement | null;
   stage = 0;
   drag_point = -35;
+  start_drag = false;
+  drag_position: {x: number, y: number} = {x: 0, y: 0};
+  stop_rotation = false;
+  button_opacity = 1;
 
   @Output() complete = false;
 
@@ -38,10 +46,10 @@ export class TidesComponent {
   }
 
   ngAfterViewInit() {
+    console.log(this.frame);
     this.wrapper = document.getElementById('wrapper');
-    this.frame   = document.getElementById('frame');
     window.addEventListener("load", () => {
-      const svg = document.querySelector("#frame") as SVGSVGElement;
+      const svg = this.frame_el.nativeElement;
       if (svg) {
         svg.style.display = "none";
         requestAnimationFrame(() => {
@@ -61,9 +69,20 @@ export class TidesComponent {
     else if (this.stage == 2) {
     }
     else if (this.stage == 3) {
-      this.wrapper?.classList.remove('zoom');
-      this.frame?.classList.add('rotate');
-      this.tide_revert.nativeElement.beginElement();
+      this.button_opacity = 0;
+      this.track_cycle().then(() => {
+        this.wrapper?.classList.remove('zoom');
+        this.stop_rotation = true;
+        this.tide_revert.nativeElement.beginElement(); 
+        this.button_opacity = 1;
+      });
+    }
+    else if (this.stage == 4) {
+      this.moon_progress().then(() => {
+        this.frame_el.nativeElement.pauseAnimations();
+        this.stop_rotations();
+        this.start_drag = true;
+      });
     }
 
   }
@@ -74,19 +93,26 @@ export class TidesComponent {
     this.tide_height.nativeElement.beginElement();
     this.tide_shift.nativeElement.beginElement();
     this.tide_revert.nativeElement.beginElement();
+  }
 
-    // if (this.wrapper && this.frame) {
-    //   if (!this.wrapper.classList.contains('zoom')) {
-    //     this.wrapper.classList.add('zoom');
-    //     this.frame.classList.remove('rotate-frame');
-    //     // this.reset_animation(this.frame);
-    //   }
-    //   else  {
-    //     this.wrapper.classList.remove('zoom');
-    //     this.frame.classList.add('rotate-frame');
-    //   }
-    // }
-    // return;
+  getRotation(el: SVGSVGElement): number {
+    const style = window.getComputedStyle(el);
+    const transform = style.getPropertyValue("transform");
+  
+    if (transform === "none") return 0;
+  
+    const values = transform.match(/matrix\(([^)]+)\)/);
+    if (!values) return 0;
+    const parts = values[1].split(",").map(v => parseFloat(v.trim()));
+    const [a, b] = parts;
+    const angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+    return angle < 0 ? angle + 360 : angle;
+  }
+
+  stop_rotations() {
+    this.earth_el.nativeElement.style.animationPlayState = 'paused';
+    this.moon_el.nativeElement.style.animation = 'none';
+    this.ocean_el.nativeElement.style.animation = 'none';
   }
 
   restart_animations() {
@@ -95,9 +121,46 @@ export class TidesComponent {
     this.tide_shift?.nativeElement.beginElement();
   }
 
-  update_curve(val: number) {
-    console.log('yeah');
-    // this.drag_point = val;
+  update_coord(val: number) {
+    console.log(val);
   }
 
+  update_pixels(vals: {x: number, y: number}) {
+    setTimeout(() => {
+      this.drag_position = vals;
+    });
+  }
+
+  track_cycle(): Promise<void> {
+    return new Promise(resolve => {
+      const tracker = setInterval(() => {
+        if (this.frame_el) {
+          const angle = this.getRotation(this.frame_el.nativeElement);
+          console.log(angle);
+          if (angle < 182 && angle > 178) {
+            console.log('reached 0');
+            clearInterval(tracker);
+            resolve();
+          }
+        }
+      }, 10);
+    });
+  }
+
+  moon_progress(): Promise<void> {
+    return new Promise(resolve => {
+      const track_prog = setInterval(() => {
+        let prog = this.moon_orbit.nativeElement.ownerSVGElement!.getCurrentTime() % 10 / 10;
+        console.log(prog);
+        if (prog > .73 && prog < .75) {
+          clearInterval(track_prog);
+          resolve();
+        }
+      }, 10);
+    });
+  }
+
+  stop_animations() {
+    return;
+  }
 }
