f6ca14fe5259e572cf2c5f7f5545033504710bc9
Randall Hale <culdesac@Mick-Donald-2.local>
2025-11-13 11:58:48 -0800

    orbit init

--- src/app/data/convert.js
+++ src/app/data/convert.js
@@ -20,11 +20,22 @@ const volcanic = parse("volcanic.csv");
 const solar = parse("solar.csv");
 const global = parse("global.csv");
 
+const rough_left = parse("rough_left.csv");
+const rough_right = parse("rough_right.csv")
+const smooth_left = parse("smooth_left.csv");
+const smooth_right = parse("smooth_right.csv")
+const generic = parse("generic.csv")
+
 const output = `
 export const human = ${JSON.stringify(human, null, 2)};
 export const volcanic = ${JSON.stringify(volcanic, null, 2)};
 export const solar = ${JSON.stringify(solar, null, 2)};
 export const global = ${JSON.stringify(global, null, 2)};
+export const rough_left = ${JSON.stringify(rough_left, null, 2)};
+export const rough_right = ${JSON.stringify(rough_right, null, 2)};
+export const smooth_left = ${JSON.stringify(smooth_left, null, 2)};
+export const smooth_right = ${JSON.stringify(smooth_right, null, 2)};
+export const generic = ${JSON.stringify(generic, null, 2)}
 `;
 
 fs.writeFileSync("climate_data.js", output);
